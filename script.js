e3f6f3d0c3cae91c576ae3531db37cacc3c3cc64
Jeremy <jeremyVDLRC@gmail.com>
2020-06-25 21:57:49 +1000

    add ajax function to return data from balldontlie API and log into console

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 //id search bar
-const searchInput = document.querySelector('.input');
+// const searchInput = document.querySelector('.input');
 const searchBtn = document.querySelector('.is-info');
 // declare variable with search bar value
-const playerName = searchInput.value;
+
 
 //declare ball don't lie api variable
 
 searchBtn.addEventListener('click', function(event) {
+    let playerName = $('.input').val();
     event.preventDefault();
-    console.log(playerName);
+    console.log(playerName)
+
+    searchBallDl(playerName);
+
 })
+
+
+
+function searchBallDl(x) {
+    let queryURL = "https://www.balldontlie.io/api/v1/players?search=" + x;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        createRow(response);
+    });
+};
