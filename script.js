76156f024b998d86f37c23e9171967569fd1db69
Dan Swan <rodger.swan@gmail.com>
2020-06-30 18:39:34 +1000

    basic pseudo for ajax functions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// const APIkey = "&api_key=J6pBLGX38uKv9oFHUNmKpHbDAvpQ1r1K";
 const savedMVPs = $('#savedMVPs');
 const savePlayerBtn = $('#savePlayerBtn');
 
@@ -53,12 +54,26 @@ function savePlayer(response) {
     let savedPlayer = "";
     savedPlayer = response.data[0].first_name + " " + response.data[0].last_name;
     console.log(savedPlayer);
-    // savedPlayerDiv.text(respon)
+    // savedPlayerDiv.text(response)
 
 }
 
 // playerStats(response.id) {
+    //eg link "https://www.balldontlie.io/api/v1/season_averages?player_ids[]=237"
     // let season = "?seasons[]=enterseason"
-    // let queryURL = `https://www.balldontlie.io/api/v1/season_averyages`${response.id}
+    // let queryURL = `https://www.balldontlie.io/api/v1/season_averyages${response.id}`
+    // console.log(reponse.id)
+    // console.log(queryURL)
 
 // }
+
+
+// const queryURL =
+//   "http://api.giphy.com/v1/gifs/search?q=" + playerName + APIkey + "&limit=5";
+// $.ajax({
+//   url: queryURL,
+//   method: "GET",
+// }).then(function (response) {
+//   console.log(response);
+//   $("#currentImage").append(response);
+// });
