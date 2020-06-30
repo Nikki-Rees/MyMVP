08c7b46efe594de0ce31b654843ffbebb0634fcd
Dan Swan <rodger.swan@gmail.com>
2020-06-30 19:04:03 +1000

    playerstats function

--- script.js
+++ script.js
@@ -64,7 +64,34 @@ function savePlayer(response) {
     // let queryURL = `https://www.balldontlie.io/api/v1/season_averyages${response.id}`
     // console.log(reponse.id)
     // console.log(queryURL)
-
+// {
+//   "data": [
+//     {
+//       "games_played":37,
+//       "player_id":237,
+//       "season":2018,
+//       "min":"34:46",
+//       "fgm":9.92,
+//       "fga":19.22,
+//       "fg3m":2.05,
+//       "fg3a":5.73,
+//       "ftm":5.08,
+//       "fta":7.54,
+//       "oreb":0.95,
+//       "dreb":7.59,
+//       "reb":8.54,
+//       "ast":7.38,
+//       "stl":1.32,
+//       "blk":0.65,
+//       "turnover":3.49,
+//       "pf":1.59,
+//       "pts":26.97,
+//       "fg_pct":0.516,
+//       "fg3_pct":0.358,
+//       "ft_pct":0.674
+//     }
+//   ]
+// }
 // }
 
 
